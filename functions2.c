00b4a5733bda17b90a52cad90adb315d3ab355f8
Isaac <mugagga10@gmail.com>
2023-11-19 11:47:36 -0800

    Revert "hottest fix"
    This reverts commit ee968a878131bc6884cf45fe1404b728c03c3f0c.

--- functions2.c
+++ functions2.c
@@ -113,29 +113,3 @@ void SwaP(stack_t **stack, unsigned int line_number)
 	(*stack)->prev = numb;
 	*stack = numb;
 }
-
-/**
- * m_rotr - Rotates the bottom value to the top.
- * @stack: Pointer to the top mode.
- * @line_number: The line number.
- */
-void m_rotr(stack_t **stack, unsigned int line_number)
-{
-	stack_t *top, *bottom;
-
-	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
-		return;
-
-	top = (*stack)->next;
-	bottom = (*stack)->next;
-	while (bottom->next != NULL)
-		bottom = bottom->next;
-
-	bottom->prev->next = NULL;
-	(*stack)->next = bottom;
-	bottom->prev = *stack;
-	bottom->next = top;
-	top->prev = bottom;
-
-	(void)line_number;
-}
